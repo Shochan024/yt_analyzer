d53285c6e273778c95a37817bf08cc82652bcf9a
shochan <44964737+Shochan024@users.noreply.github.com>
2026-09-23 06:34:26 +0900

    [yt#21] expose max acceleration day to report

--- src/yt_analyzer/report/data.py
+++ src/yt_analyzer/report/data.py
@@ -32,6 +32,7 @@ class ReportDataBuilder:
     "reacceleration_count",
     "primary_reacceleration_day",
     "primary_reacceleration_views",
+    "primary_reacceleration_peak_strength_day",
     "primary_reacceleration_strength",
     "cumulative_views_3d",
     "cumulative_views_7d",
