52f5d7607df2027696d375e0ca9ceccf16e8e679
石津聖 <jadetech0024@gmail.com>
2026-09-23 04:08:21 +0900

    [yt#15] ♻️  linter修正

--- tasks.py
+++ tasks.py
@@ -3,12 +3,12 @@
 from invoke import Collection, task
 
 from src.yt_analyzer.application.performance_metrics_command import (
-  run_performance_metrics
+  run_performance_metrics,
 )
 from src.yt_analyzer.application.pipeline_command import run_all_pipeline
 from src.yt_analyzer.application.statistics_command import (
   run_long_statistics,
-  run_short_statistics
+  run_short_statistics,
 )
 from src.yt_analyzer.application.title_features_command import run_title_features
 
